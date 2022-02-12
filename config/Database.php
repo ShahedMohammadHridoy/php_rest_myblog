72f5012e73d3ca36fa54aec17b3c96cb8760a282
Shahed <shahed_hridoy@outlook.com>
2022-02-12 12:45:24 +0600

    veriable declared

--- config/Database.php
+++ config/Database.php
@@ -1,5 +1,12 @@
 <?php
     class Database{
         // DB Params
+        private $host = 'localhost';
+        private $db_name = 'myblog';
+        private $username = 'root';
+        private $password = '';
+        private $conn;
+
+        
 
     }
